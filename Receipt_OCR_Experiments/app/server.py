c1beae9d9d1e37520e8278ed82fb77c73958cc26
Aman <47506369+amanguptaBay@users.noreply.github.com>
2024-04-20 09:35:59 -0700

    Completly functional server

--- Receipt_OCR_Experiments/app/server.py
+++ Receipt_OCR_Experiments/app/server.py
@@ -5,13 +5,11 @@ import base64
 from flask_cors import CORS
 import pytesseract
 import os
+import a
 
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part, FinishReason
 import vertexai.preview.generative_models as generative_models
-
-GOOGLE_API_KEY = os.environ['GOOGLE_API_KEY']
-genai.configure(api_key=GOOGLE_API_KEY)
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -24,10 +22,7 @@ def upload_image():
     image_data = base64.b64decode(base64_image)
     image = Image.open(io.BytesIO(image_data))
     text = pytesseract.image_to_string(image, config="-l eng --psm 3")
-    print(text)
-    model = genai.GenerativeModel('gemini-pro')
-    llm_response = model.generate_content(f"{text} This is a OCR of a receipt. Please extract in JSON form: the store name, total, address of store, along with the items their unit prices, total price, amount purchased, brand name, item name, item name full form (inferred). Any unknown item please write null instead of guessing
-")
+    llm_response = a.generate(text)
     return jsonify({'ocr': text, "llm": llm_response})
 
 if __name__ == '__main__':
