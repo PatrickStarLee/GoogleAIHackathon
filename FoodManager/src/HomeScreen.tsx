c81c4db156f766d0b5d539e3e0e7aa7d42e32015
PatrickStarLee <patricklee260@gmail.com>
2024-06-23 12:37:14 -0700

    added google-oauth feature in the home screen

--- FoodManager/src/HomeScreen.tsx
+++ FoodManager/src/HomeScreen.tsx
@@ -1,81 +1,95 @@
 import {
-    View,
-    StyleSheet,
-    TextInput,
-    Text,
-    Pressable,
-    TouchableOpacity,
-    Image,
-    Button,
-    FlatList
-  } from "react-native";
-  import React, { useState } from "react";
-  import { SearchBar, ListItem } from 'react-native-elements';
-  import { Ionicons } from '@expo/vector-icons';
-  import Icon from 'react-native-vector-icons/FontAwesome';
-  import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { useNavigation } from '@react-navigation/native';
+  View,
+  StyleSheet,
+  TextInput,
+  Text,
+  Pressable,
+  TouchableOpacity,
+  Image,
+  Button,
+  FlatList,
+} from "react-native";
+import React, { useState } from "react";
+import { SearchBar, ListItem } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { useNavigation } from "@react-navigation/native";
 import { InventoryPage } from "./InventoryPage";
 import { ReceiptImageUpload } from "./ReceiptImageUpload";
 import { CreateAndEditRecipes } from "./CreateAndEditRecipes";
 import { CompareRecipes } from "./CompareRecipes";
 import { FindRecipesPage } from "./FindRecipesPage";
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { auth } from "../Firebase/config"
 
-const HomeScreen = ({navigation}) => {
+
+const HomeScreen = ({ navigation }) => {
+  const handleGoogle = async (e) => {
+    const provider = await new GoogleAuthProvider();
+    return signInWithPopup(auth, provider)
+  }
 
   return (
-    
-  
-  <View style={styles.middle}>
-    <Text> Home Screen </Text>
-    <View style={styles.button}>
-      <Pressable 
-        style={{backgroundColor: 'blue', padding: 10, alignItems: 'center'}}
-        onPress={() => navigation.navigate('CreateAndEditRecipes')}
+    <View style={styles.middle}>
+      <Text> Home Screen </Text>
+      <View style={styles.button}>
+        <Pressable
+          style={{ backgroundColor: "red", padding: 10, alignItems: "center" }}
+          onPress={handleGoogle}
+        >
+          <Text style={{ color: "white" }}>Google Sign In</Text>
+        </Pressable>
+      </View>
+      <View style={styles.button}>
+        <Pressable
+          style={{ backgroundColor: "blue", padding: 10, alignItems: "center" }}
+          onPress={() => navigation.navigate("CreateAndEditRecipes")}
+        >
+          <Text style={{ color: "white" }}>Create and Edit Recipes</Text>
+        </Pressable>
+      </View>
+      <View style={styles.button}>
+        <Pressable
+          style={{
+            backgroundColor: "green",
+            padding: 10,
+            alignItems: "center",
+          }}
+          onPress={() => navigation.navigate("CompareRecipes")}
+        >
+          <Text style={{ color: "white" }}>Compare Recipes</Text>
+        </Pressable>
+      </View>
+      <View style={styles.button}></View>
+      <Pressable
+        style={{ backgroundColor: "black", padding: 10, alignItems: "center" }}
+        onPress={() => navigation.navigate("FindRecipesPage")}
       >
-        <Text style={{color: 'white'}}>Create and Edit Recipes</Text>
+        <Text style={{ color: "white" }}>Find Recipes</Text>
       </Pressable>
     </View>
-    <View style={styles.button}>
-      <Pressable 
-        style={{backgroundColor: 'green', padding: 10, alignItems: 'center'}}
-        onPress={() => navigation.navigate('CompareRecipes')}
-      >
-        <Text style={{color: 'white'}}>Compare Recipes</Text>
-      </Pressable>
-    </View>
-    <View style={styles.button}>
-    </View>
-      <Pressable 
-        style={{backgroundColor: 'black', padding: 10, alignItems: 'center'}}
-        onPress={() => navigation.navigate('FindRecipesPage')}
-      >
-        <Text style={{color: 'white'}}>Find Recipes</Text>
-      </Pressable>
-  </View>
-  
- 
   );
 };
 
-  const styles = StyleSheet.create({
-    middle: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      padding: 10,
-    },
-    button: {
-      borderRadius: 50,
-      margin: 10,
-    },
-  });
+const styles = StyleSheet.create({
+  middle: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 10,
+  },
+  button: {
+    borderRadius: 50,
+    margin: 10,
+  },
+});
 
-  export {HomeScreen};
+export { HomeScreen };
